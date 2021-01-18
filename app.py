f08db3214df32087f3ac597adbd4501119f12af9
Brian Naughton <bpnaughton@yahoo.co.uk>
2021-01-18 09:36:26 +0000

    picamera img working

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
-#from tank.controller import Controller
+from tank.controller import Controller
 from tank.camera import ImageCapture
 import logging
 import sys
@@ -14,10 +14,11 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 #controller = Controller()
+imageCapture=ImageCapture()
 imageCaptureThread=None
 
 #turn the flask app into a socketio app
-socketio = SocketIO(app)
+socketio = SocketIO(app,logger=True,engine_logger=True,async_mode='gevent')
 
 @app.route('/')
 def mainRoute():
@@ -34,27 +35,39 @@ def dashboardRoute():
   logger.debug("dashboard route")
   return render_template('dashboard.html')
 
+def sendImage():
+  logger.debug('getting image from camera')
+  while True:
+    image=imageCapture.getNextEncodedImage()
+    logger.debug('sending image to client')
+    if image is not None:
+      socketio.emit('image', image, broadcast=True)
+      socketio.sleep(1)
+    else:
+      logger.debug('null image')
+
 @socketio.on('connect')
 def controller_connect():
   logger.debug('Controller client connected')
-  imageCaptureThread = ImageCapture(socketio)
-  imageCaptureThread.run()
-
+  global imageCaptureThread
+  if imageCaptureThread is None:
+    logger.debug('starting image thread')
+    imageCaptureThread = socketio.start_background_task(target=sendImage)
+ 
 @socketio.on('move')
 def handle_controller_move_message(data):
   logger.debug('received move message: ' + str(data))
-  #d = json.loads(data)
-  controller.move(data)
+#  controller.move(data)
 
 @socketio.on('stop')
 def handle_controller_stop_message(data):
   logger.debug('received stop message')
-  controller.stop()
+#  controller.stop()
 
 @socketio.on('disconnect')
 def controller_disconnect():
   logger.debug('Controller client disconnected')
-  controller.stop()
+#  controller.stop()
 
 if __name__ == "__main__":
   logger.info('Starting socketio')
--- tank/camera.py
+++ tank/camera.py
@@ -3,16 +3,14 @@ from picamera import PiCamera
 import time 
 import cv2 # OpenCV library
 import logging
-from threading import Thread
 import base64
+import io
 
-class ImageCapture(Thread):
-  def __init__(self, socketio):
+class ImageCapture():
+  def __init__(self):
     super().__init__()
     self.logger = logging.getLogger(__name__)
     self.logger.debug('Init image capture')
-
-    self.socketio=socketio
 
     # Initialize the camera
     self.camera = PiCamera() 
@@ -21,7 +19,9 @@ class ImageCapture(Thread):
     # Set the number of frames per second
     self.camera.framerate = 32 
     # Generates a 3D RGB array and stores it in rawCapture
-    self.raw_capture = PiRGBArray(camera, size=(640, 480))
+    self.raw_capture = PiRGBArray(self.camera, size=(640, 480))
+#    self.raw_capture = io.BytesIO()
+
     # Wait a certain number of seconds to allow the camera time to warmup
     time.sleep(0.1)
     # current image
@@ -29,40 +29,22 @@ class ImageCapture(Thread):
     # loop bool
     self.running=True
 
-  def run(self):
-    self.logger('entering run loop')
-    # collect images and send to 
-    self.capture()
-
-  def stop(self):
-    self.logger('stopping image capture thread')
-    self.running=False
-
   def capture(self):
-    self.logger.debug('Start capturing video')
+    self.logger.debug('Capturing next frame')
     # Capture frames continuously from the camera
-    for frame in camera.capture_continuous(self.raw_capture, format="bgr", use_video_port=True):        
-      # Grab the raw NumPy array representing the image
-      self.image = frame.array
-      # Display the frame using OpenCV
-      # cv2.imshow("Frame", self.image)
+    self.camera.capture(self.raw_capture, format="bgr", use_video_port=True)
+    res, self.image=cv2.imencode('.JPEG', self.raw_capture.array)
 
-      # send the image to connected clients
-      self.sendImage()      
+    # self.logger.debug(self.image.tobytes())
+    # Clear the stream in preparation for the next frame
+    self.raw_capture.truncate(0)
 
-      # Clear the stream in preparation for the next frame
-      self.raw_capture.truncate(0)
-      if self.running is False:
-        self.camera.close()
-        break
-
-  def sendImage(self):
-    self.logger('sending image')
-    socketio.emit('image', {'buffer': base64.b64encode(self.image) } )
-
-  def getImage(self):
-    self.logger.debug('Return current image')
-    return self.image
+  def getNextEncodedImage(self):
+    self.logger.debug('returning current image encoded')
+    self.capture()
+    if self.image is not None:
+      return base64.b64encode(self.image.tobytes()).decode('utf-8')
+    return None
 
 class AnalyseImages:
   def __init__(self):
